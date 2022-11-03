6454fa0d062f5e01e933f8e99e33ff752bbe8578
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-03 23:12:58 +0330

    add state

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import Header from '../../Components/Header/Header';
 import Card from '../../Components/Card/CardHome'
 import options from '../../Api/Api';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
 function HomePage() {
     const [dataCoins, setDataCoins] = useState([])
     const handleData = async () => {
@@ -14,22 +16,27 @@ function HomePage() {
     useEffect(() => {
         handleData()
     }, [])
+    const handelCheck = uuid => {
+        setDataCoins(dataCoins.map(coin => coin.uuid === uuid ? { ...coin, lVolume: !coin.lVolume } : coin))
+    }
     return (
         <Grid>
             <Header />
-            <Card dataCoins={dataCoins}/>
+            <Card dataCoins={dataCoins} />
             {
-                dataCoins.map((item,index)=>
+                dataCoins.map((item, index) =>
                     <Grid key={index}>
-                    <img width={'100px'} src={item.iconUrl} />
-                    <h3>{item.symbol}</h3>
-                    <h3>{item.name}</h3>
-                    <h3>{item.price}</h3>
-                    <h3>{item.change}</h3>
-                    <h3>{item.marketCap}</h3>
-
-                </Grid>
-                    )
+                        <img width={'100px'} src={item.iconUrl} />
+                        <h3>{item.symbol}</h3>
+                        <h3>{item.name}</h3>
+                        <h3>{item.price}</h3>
+                        <h3>{item.change}</h3>
+                        <h3>{item.marketCap}</h3>
+                        <Grid onClick={() => handelCheck(item.uuid)}>
+                            <h3>{item.lVolume ? <StarIcon /> : <StarBorderIcon />}</h3>
+                        </Grid>
+                    </Grid>
+                )
             }
         </Grid>
     );
