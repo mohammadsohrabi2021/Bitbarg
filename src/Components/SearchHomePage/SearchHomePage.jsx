6b2ee4f3d0bd87e10de01e65113275aaaa9674c2
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-08 03:00:23 +0330

    change component create componenet search

---
+++ src/Components/SearchHomePage/SearchHomePage.jsx
@@ -0,0 +1,24 @@
+import { Grid, IconButton, InputBase } from '@mui/material';
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+function SearchHomePage({setSearch,search}) {
+    const handleSearch = e => {
+        setSearch(e.target.value)
+    }
+    return (
+        <Grid item sx={{ border: '1px solid #727272 ', borderRadius: '10px' }} xs={12}>
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+            <InputBase sx={{ ml: 1, flex: 1 }}
+                placeholder="جستجو"
+                onChange={handleSearch}
+                value={search}
+                name='search'
+                inputProps={{ 'aria-label': 'search google maps' }}
+            />
+        </Grid>
+    );
+}
+
+export default SearchHomePage;
