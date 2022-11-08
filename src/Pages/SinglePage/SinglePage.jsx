4d2983b95ea8fbe09d7ebb05f2088d2ea676ea25
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-08 13:29:33 +0330

    change singlePage

--- src/Pages/SinglePage/SinglePage.jsx
+++ src/Pages/SinglePage/SinglePage.jsx
@@ -1,49 +1,58 @@
-import React, { useState,useContext  } from 'react';
-import { Container, Grid } from '@mui/material';
-import Filter from '../../Components/Filter/Filter';
+import React, { useState, useContext, useEffect } from 'react';
+import { Container, FormControlLabel, Grid, Switch } from '@mui/material';
+// import Filter from '../../Components/Filter/Filter';
 import FilterPrice from '../FilterPrice/FilterPrice';
 import HeaderSinglePage from '../HeaderSinglePage/HeaderSinglePage';
 import DataTitle from '../DataTitle/DataTitle';
 import CardPageData from '../CardPageData/CardPageData';
-import { IconButton, InputBase } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
 import { DataContext } from '../../Context/CoinProvider';
+import SearchHomePage from '../../Components/SearchHomePage/SearchHomePage';
 function SinglePage() {
-    const [filter, setFilter] = useState('favoriteData')
-    const {dataCoins, setDataCoins } =useContext(DataContext)
-    const handleFilterBtn = title => {
-        setFilter(title)
-    }
+
     const [search, setSearch] = useState('')
-    const handleSearch = e => {
-        setSearch(e.target.value)
-    }
+    const [unit, setUnit] = useState(true);
+    const { dataCoins, setDataCoins } = useContext(DataContext)
+    const [status, setStatus] = useState(false);
+    const [filtered, setFiltered] = useState([]);
+    const handleStar = (id) => {
+        setDataCoins(
+            dataCoins.map((coin) =>
+                coin.uuid === id ? { ...coin, star: !coin.star } : coin
+            )
+        );
+    };
+    const handleStatus = () => {
+        if (status) {
+            setFiltered(dataCoins.filter((coin) => coin.star === true));
+        } else {
+            setFiltered(dataCoins);
+        }
+    };
+    useEffect(() => {
+        handleStatus();
+    }, [status, dataCoins, filtered]);
     return (
         <Grid>
             <Container sx={{ background: '#fff', boxShadow: 2, borderRadius: 3 }}>
                 <DataTitle dataCoins={dataCoins} />
                 <Grid container item pt={5}>
-                    <Grid item sx={{ border: '1px solid #727272 ', borderRadius: '10px' }} >
-                        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                            <SearchIcon />
-                        </IconButton>
-                        <InputBase sx={{ ml: 1, flex: 1 }}
-                            placeholder="جستجو"
-                            onChange={handleSearch}
-                            value={search}
-                            name='search'
-                            inputProps={{ 'aria-label': 'search google maps' }}
-                        />
+                    <Grid>
+                        <SearchHomePage search={search} setSearch={setSearch} />
                     </Grid>
                     <Grid mx={2}>
-                        <Filter handleFilterBtn={handleFilterBtn} />
+                        <FormControlLabel
+                            control={<Switch onChange={() => setStatus(!status)} />}
+                            label="نشان شده ها"
+                            labelPlacement="Left"
+                        />
+                        {/* <Filter handleFilterBtn={handleFilterBtn} /> */}
                     </Grid>
-                    <FilterPrice  />
+                    <FilterPrice />
                 </Grid>
                 <Grid mt={5}>
                     <HeaderSinglePage />
                     <Grid>
-                        <CardPageData search={search} dataCoins={dataCoins} filter={filter} setDataCoins={setDataCoins} />
+                        <CardPageData search={search} dataCoins={dataCoins} status={status} setDataCoins={setDataCoins} />
                     </Grid>
                 </Grid>
             </Container>
