95c07aa8fe1b96dfc5553850a773d8ec201c718c
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-11 22:07:36 +0330

    add dainamic title

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Grid } from '@mui/material';
+import { useEffect } from 'react';
 import Card from '../../Components/Card/CardHome'
 
-
 function HomePage() {
- 
+    useEffect(() => {
+        document.title = "بیت برگ |خرید بیت کوین , فروش بیت کوین , قیمت بیت کوین"
+    }, [])
     return (
         <Grid>
             <Card/>
--- src/Pages/SinglePage/SinglePage.jsx
+++ src/Pages/SinglePage/SinglePage.jsx
@@ -1,9 +1,11 @@
 import { Container, Paper } from '@mui/material';
 import NavbarPrice from '../Navbar/NavbarPrice';
 import {styles} from'./BackGround'
+import { useEffect } from 'react';
 function SinglePage() {
-   
-
+  useEffect(()=>{
+    document.title="قیمت لحظه ای | بیت برگ"
+  })
     return ( 
         <Paper style={styles.paperContainer}>
             <Container>
